537380da0f5ce94869d3b971edab6311fcb3b2e8
Eric <eric.vacquet06@gmail.com>
2021-08-25 02:12:17 +0200

    First commit

---
+++ models/Sauce.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    heat: { type: Number, required: true },
+    userId: {type: String, required: true},
+    likes: {type: Number, required: false},
+    dislikes: {type: Number, required: false},
+    usersLikes: {type: String, required: false},
+    usersDislikes: {type: String, required: false}
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema);
