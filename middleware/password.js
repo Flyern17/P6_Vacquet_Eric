1e19e6bf3d2d5d513f87d0c68e7a472ba5f157db
Eric <eric.vacquet06@gmail.com>
2021-09-06 01:46:53 +0200

    Last commit

--- middleware/password.js
+++ middleware/password.js
@@ -2,7 +2,7 @@ const passwordSchema = require('../models/Password');
 
 module.exports = (req, res, next) => {
     if(!passwordSchema.validate(req.body.password)) {
-        res.writeHead(400, 'Le mot de passe doit contenir entre 8 et 100 caractères, et également avoir une majuscule');
+        res.writeHead(400, 'Le mot de passe doit contenir entre 8 et 100 caractères, avoir une majuscule et contenir au moins 2 chiffres!');
         res.end('Le format du mot de passe est incorrect !');
     } else {
         next();
--- middleware/rate-limiter.js
+++ middleware/rate-limiter.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 10, // Nombre de requetes possibles pour chaque adresse IP
-    message: "Vous avez fait trop de tentatives de connexion, re-tentez dans 15m"
+    message: "Nombre de requêtes dépassés, re-tentez dans 15m"
 });
 
 module.exports = limiter;
